6a66139ea3c11b516d79b6b63df3274e87118bbe
BubbleCoinLOL <35410240+BubbleCoinLOL@users.noreply.github.com>
2018-01-14 10:26:45 -0500

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,53 +1,24 @@
 pragma solidity ^0.4.19;
 
-contract owned {
-    address public owner;
-
-    function owned() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
-    }
-}
-
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-contract BubbleCoin is owned{
+contract BubbleCoin {
     // Public variables of the token
     string public name;
     string  public symbol;
     uint8 public decimals = 3;
     uint256 public totalSupply;
-
     // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
-
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
-    /**
-     * Constrctor function
-     *
-     * Initializes contract with initial supply tokens to the creator of the contract
-     */
-    function BubbleCoin(
-    ) public {
+    function BubbleCoin() public {
         totalSupply = 100000000 * 10 ** uint256(decimals);  // Update total supply with the decimal amount
         balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
         name = "BubbleCoin";                                   // Set the name for display purposes
         symbol = "BBL";                               // Set the symbol for display purposes
     }
-
-    /**
-     * Internal transfer, only can be called by this contract
-     */
     function _transfer(address _from, address _to, uint _value) internal {
         // Prevent transfer to 0x0 address. Use burn() instead
         require(_to != 0x0);
@@ -74,14 +45,11 @@ contract BubbleCoin is owned{
         _transfer(_from, _to, _value);
         return true;
     }
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         return true;
     }
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        public
-        returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
         tokenRecipient spender = tokenRecipient(_spender);
         if (approve(_spender, _value)) {
             spender.receiveApproval(msg.sender, _value, this, _extraData);
